3915f31415bf3c0e105f4ed65b9d25f6dbf98829
1z3r091 <1z3r091@gmail.com>
2022-10-16 01:45:01 +0900

    Fix ProductChange

---
+++ src/components/ProductField.js
@@ -0,0 +1,18 @@
+import ProductList from './ProductList';
+import Search from './Search';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function ProductField({ products }) {
+  const [state, setState] = useState({
+    searchText: '',
+    inStock: false,
+  });
+  return (
+    <>
+      <Search state={state} setState={setState} />
+      <ProductList products={products} searchText={state.searchText} inStock={state.inStock} />
+      <Link to='/blog'>Blog</Link> | <Link to='/tech'>Tech</Link>
+    </>
+  );
+}
